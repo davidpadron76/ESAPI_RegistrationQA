93d943df8c869f4958a8af2a00cb22d23e5c51c8
Alvaro Padron <112880696+davidpadron76@users.noreply.github.com>
2026-07-25 19:38:00 -0500

    Add files via upload

---
+++ UI/MainControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using ESAPI_RegistrationQA.ViewModels;
+
+namespace ESAPI_RegistrationQA.UI
+{
+    public partial class MainControl : UserControl
+    {
+        public MainControl(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
